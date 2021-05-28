03288534a5b0b1cfe902227eba9a5449f761474b
Andre Volel <avolel@gmail.com>
2021-05-28 11:18:54 -0400

    Add project files.

---
+++ ConsoleUI/GreetingService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class GreetingService : IGreetingService
+    {
+        private readonly ILogger<GreetingService> log;
+        private readonly IConfiguration config;
+
+        public GreetingService(ILogger<GreetingService> log, IConfiguration config)
+        {
+            this.log = log;
+            this.config = config;
+        }
+
+        public void Run()
+        {
+            for (int i = 0; i < config.GetValue<int>("LoopTimes"); i++)
+            {
+                log.LogInformation("Run Number {runNumber}", i);
+            }
+        }
+    }
+}
